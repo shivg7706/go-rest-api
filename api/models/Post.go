43b1f87e0f0aade6f140b6bbc738d27ceeba864b
Shivam Gupta <shivamgupta7706@gmail.com>
2020-12-31 19:16:31 +0530

    feat: for other func

--- api/models/Post.go
+++ api/models/Post.go
@@ -89,3 +89,33 @@ func (p *Post) FindPostByID(db *gorm.DB, pid uint64) (*Post, error) {
 	}
 	return p, nil
 }
+
+func (p *Post) UpdateAPost(db *gorm.DB) (*Post, error) {
+
+	var err error
+
+	err = db.Debug().Model(&Post{}).Where("id = ?", p.ID).Updates(Post{Title: p.Title, Content: p.Content, UpdatedAt: time.Now()}).Error
+	if err != nil {
+		return &Post{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.AuthorID).Take(&p.Author).Error
+		if err != nil {
+			return &Post{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Post) DeleteAPost(db *gorm.DB, pid uint64, uid uint32) (int64, error) {
+
+	db = db.Debug().Model(&Post{}).Where("id = ? and author_id = ?", pid, uid).Take(&Post{}).Delete(&Post{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Post not found")
+		}
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
