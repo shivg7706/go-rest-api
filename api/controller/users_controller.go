8154144362ab0b64220c534a5b050375ca9d989a
Shivam Gupta <shivamgupta7706@gmail.com>
2020-12-31 14:41:01 +0530

    feat: get user/s

--- api/controller/users_controller.go
+++ api/controller/users_controller.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/shivg7706/rest-api/api/responses"
 	"github.com/shivg7706/rest-api/api/utils/formaterror"
 	"github.com/shivg7706/rest-api/models"
@@ -40,4 +42,33 @@ func (server *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, userCreated.ID))
 	responses.JSON(w, http.StatusCreated, userCreated)
+}
+
+func (server *Server) GetUsers(w http.ResponseWriter, r *http.Request) {
+
+	user := models.User{}
+
+	users, err := user.FindAllUsers(server.DB)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, users)
+}
+
+func (server *Server) GetUser(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	user := models.User{}
+	userGotten, err := user.FindUserByID(server.DB, uint32(uid))
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, userGotten)
 }
