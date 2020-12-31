10e8383e0c6dddc77d0d403309ad9e0cac7b30e3
Shivam Gupta <shivamgupta7706@gmail.com>
2020-12-31 13:44:18 +0530

    feat: create usert

---
+++ api/controller/users_controller.go
@@ -0,0 +1,43 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/shivg7706/rest-api/api/responses"
+	"github.com/shivg7706/rest-api/api/utils/formaterror"
+	"github.com/shivg7706/rest-api/models"
+)
+
+func (server *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user.Prepare()
+	err = user.Validate("")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	userCreated, err := user.SaveUser(server.DB)
+
+	if err != nil {
+
+		formattedError := formaterror.FormatError(err.Error())
+
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, userCreated.ID))
+	responses.JSON(w, http.StatusCreated, userCreated)
+}
